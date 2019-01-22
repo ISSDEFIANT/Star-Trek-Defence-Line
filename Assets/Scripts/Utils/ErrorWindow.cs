00bd4ca69353a6c0ee98ec46766b27ef36c34196
alesharik <alesharikReserv@yandex.ru>
2019-01-22 23:14:26 +0300

    Add some events to ErrorWindow

--- Assets/Scripts/Utils/ErrorWindow.cs
+++ Assets/Scripts/Utils/ErrorWindow.cs
@@ -8,17 +8,27 @@ public sealed class ErrorWindow : MonoBehaviour
     private static bool _messageWindowActive;
     private static string _curMessage;
 
+    public delegate void ErrorHandler(string message);
+
+    public delegate void WindowActionHandler();
+
+    public static event ErrorHandler OnError = message => {};
+    public static event WindowActionHandler OnWindowOpen = () => {};
+    public static event WindowActionHandler OnWindowClose = () => {};
+
     /// <summary>
     /// Show error message to the user
     /// </summary>
     /// <param name="message">the message</param>
     public static void ShowErrorMessage(string message)
     {
+        OnError(message);
         lock (Lock)
         {
             _curMessage = message;
             _messageWindowActive = true;
         }
+        OnWindowOpen();
     }
 
     /// <summary>
@@ -26,15 +36,17 @@ public sealed class ErrorWindow : MonoBehaviour
     /// </summary>
     /// <param name="e">the exception</param>
     public static void ShowException(Exception e)
-    {
+    {   
+        OnError(e.ToString());
         lock (Lock)
         {
             _curMessage = e.ToString();
             _messageWindowActive = true;
         }
+        OnWindowOpen();
     }
 
-    void OnGUI()
+    private void OnGUI()
     {
         string msg;
         lock (Lock)
@@ -64,12 +76,15 @@ public sealed class ErrorWindow : MonoBehaviour
         GUI.color = Color.red;
         GUI.Box(new Rect(x * 25, y * 5, x * 60, y * 90), "Error", box);
         GUI.Label(new Rect(x * 30, y * 10, x * 50, y * 5),
-            "Произошла ошибка. Пожалуйста, напишите нам о ней в багтрекер:", label);
+            "Error happened. This may cause instability in your game. Please, report it using bugtracker:", label);
         GUI.Label(new Rect(x * 30, y * 15, x * 50, y * 80), msg, button);
         GUI.color = Color.white;
         if (GUI.Button(new Rect(x * 30, y * 85, x * 5, y * 3), "Close", button))
             lock (Lock)
+            {
                 _messageWindowActive = false;
+                OnWindowClose();
+            }
 
         if (GUI.Button(new Rect(x * 60, y * 85, x * 20, y * 5), "Open bugtracker", button))
             Application.OpenURL("https://github.com/ISSDEFIANT/Star-Trek-Defence-Line/issues");
